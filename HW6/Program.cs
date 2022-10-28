f7acf67ae7c0b6649897e347b91bb51aeacf73a0
Ilya <ilya.romanov.1977@bk.ru>
2022-10-28 19:00:28 +0300

    Добавьте файлы проекта.

---
+++ HW6/Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Судебные рабирательства: 
+ *  -Суд или судья прокурор следователь дознаватель(органы госудраства должностные лица)
+ *  -подозреваемый адвокат ответчик потерпевший гражданский истец(участники уголовного процесса)
+ *  -Присяжные свидетели(привелкаемые лица)
+ * инкапсуляция, наследование, полиморфизм
+ 
+ */
+namespace HW6
+{
+    //public class Prestupnik
+    //{
+    //    public string f_name;
+    //    public string F_name
+    //    {
+    //        get { return f_name; }
+    //        set { f_name = value; }
+    //    }
+    //    public string s_name;
+    //    public string S_name
+    //    {
+    //        get { return s_name; }
+    //        set { s_name = value; }
+    //    }
+    //    public int age;
+    //    public int Age
+    //    {
+    //        get { return age; }
+    //        set { age = value; }
+    //    }
+    //    public void GetInfo(string[] file)
+    //    {
+    //        f_name = file[0];
+    //        s_name = file[1];
+    //        age = int.Parse(file[2]);
+    //    }
+    //    public void SetInfo()
+    //    {
+    //        Console.WriteLine("Информация о преступнике:");
+    //        Console.WriteLine($"Name: {f_name} {s_name}, Age: {age}");
+    //    }
+    //    public void SLova() { }
+    //}
+    //public class Poterpevshiy
+    //{
+    //    public string f_name;
+    //    public string F_name
+    //    {
+    //        get { return f_name; }
+    //        set { f_name = value; }
+    //    }
+    //    public string s_name;
+    //    public string S_name
+    //    {
+    //        get { return s_name; }
+    //        set { s_name = value; }
+    //    }
+    //    public int age;
+    //    public int Age
+    //    {
+    //        get { return age; }
+    //        set { age = value; }
+    //    }
+    //    public void GetInfo(string[] file)
+    //    {
+    //        f_name = file[0];
+    //        s_name = file[1];
+    //        age = int.Parse(file[2]);
+    //    }
+    //    public void SetInfo()
+    //    {
+    //        Console.WriteLine("Информация о потерпевшем:");
+    //        Console.WriteLine($"Name: {f_name} {s_name}, Age: {age}");
+    //    }
+    //}
+    //public class Prestuplenie
+    //{
+    //    public string sostav;
+    //    public string Sostav
+    //    {
+    //        get { return sostav; }
+    //        set { sostav = value; }
+    //    }
+    //    public void GetS(string[] file)
+    //    {
+    //        sostav = file[0];
+    //    }
+    //}
+    //abstract class Process
+    //{
+    //    public int id;
+    //    public int ID
+    //    {
+    //        get { return id; }
+    //        set { id = value; }
+    //    }
+    //    public void Rnadom_ID()
+    //    {
+    //        Random r = new Random();
+    //        id = r.Next(0, 1000);
+    //    }
+    //}
+    //class Zasedanie : Process
+    //{
+    //    public string name;
+    //    public string Name
+    //    {
+    //        get { return name; }
+    //        set { name = value; }
+    //    }
+    //    public Zasedanie() { }
+    //    public void GI(string[] file)
+    //    {
+    //        Name = file[0];
+    //    }
+    //    public void Nachalo_Zasedaniya()
+    //    {
+    //        Console.WriteLine($"Всем встать! Суд идёт. Сегодня рассматривается дело №{id} на имя {Name}");
+    //    }
+    //}
+    //class Prigovor : Process
+    //{
+    //}
+    //class Reshenie_Prisyajhnih : Process
+    //{
+    //}
+    public class Incapsulation
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Enter name");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter age");
+            Age = Convert.ToInt32(Console.ReadLine());
+        }
+        public void SetInfo()
+        {
+            Console.WriteLine($"Name is {Name}, Age is {Age}");
+        }
+    }
+    public class NasledovanieAndPolomorphism
+    {
+        public string shape { get; set; }
+        public string size { get; set; }
+        public void GetInfo()
+        {
+            Console.WriteLine("Enter shape");
+            shape = Console.ReadLine();
+            Console.WriteLine("Enter size");
+            size = Console.ReadLine();
+        }
+        public virtual void Info()
+        {
+            Console.WriteLine("New figure");
+        }
+    }
+    public class f1 : NasledovanieAndPolomorphism
+    {
+        public string color = "Red";
+        public override void Info()
+        {
+            Console.WriteLine("Figure is {0}, color is {1}, size is {2}", shape, color, size);
+        }
+    }
+    public class f2 : NasledovanieAndPolomorphism
+    {
+        public string color = "Green";
+        public override void Info()
+        {
+            Console.WriteLine("Color of figure is {1}, shape is {0} and the size is {2}", shape, color, size);
+        }
+    }
+    public class Construktor
+    {
+        public int speed;
+        public string type;
+        public byte wheels;
+        public Construktor() { speed = 0; type = "Broken Car"; wheels = 1; }
+        public Construktor(int n) { speed = n; type = "Truck"; wheels = 6; }
+        public Construktor(int n, string n2) { speed = n; type = n2; wheels = 4; }
+        public Construktor(int n, string n2, byte n3) { speed = n; type = n2; wheels = n3; }
+        public void Print()
+        {
+            Console.WriteLine($"SPEED - {speed} , TYPE - {type} , WHEELS - {wheels}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prestupnik p = new Prestupnik();
+            //Poterpevshiy py = new Poterpevshiy();
+            //Zasedanie z = new Zasedanie();
+            //z.Rnadom_ID();
+            //z.Nachalo_Zasedaniya();
+            //Console.ReadKey();
+            Incapsulation n1 = new Incapsulation();
+            n1.GetInfo();
+            n1.SetInfo();
+            NasledovanieAndPolomorphism n2 = new NasledovanieAndPolomorphism();
+            n2.Info();
+            f1 n3 = new f1();
+            n3.GetInfo();
+            n3.Info();
+            f2 n4 = new f2();
+            n4.GetInfo();
+            n4.Info();
+            Construktor c1 = new Construktor();
+            Construktor c2 = new Construktor(60);
+            Construktor c3 = new Construktor(100, "Sport-car");
+            Construktor c4 = new Construktor(300, "Bike", 2);
+            c1.Print();
+            c2.Print();
+            c3.Print();
+            c4.Print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tumakov7/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumakov7
+{
+    public class Bank_Account
+    {
+        private byte ID { get; set; }
+        private decimal Balance { get; set; }
+        public enum Acc_Type: byte
+        {
+            Сберегательный,
+            Накопительный
+        }
+        private Acc_Type Type { get; set; }
+        private static HashSet<byte> Last_Random_ID = new HashSet<byte>( 0 );
+
+        public Bank_Account() { }
+        public Bank_Account(byte iD, decimal balance, Acc_Type type)
+        {
+            ID = iD;
+            Balance = balance;
+            Type = type;
+        }
+        //public void Input(out byte id, out decimal balance, out Bank_Account.Acc_Type acc_Type)
+        //{
+        //    id = Convert.ToByte(Console.ReadLine());
+        //    balance = Convert.ToByte(Console.ReadLine());
+        //    byte acc = Convert.ToByte(Console.ReadLine());
+        //    acc_Type = Enum.Get;
+        //}
+        public void Print() => Console.WriteLine($"Id: {ID}\nBalace: {Balance}\nType: {Type}");
+        public byte new_ID()
+        {
+            Random r = new Random();
+            ID = (byte)r.Next(0, 255);
+            if (!Last_Random_ID.Contains(ID))
+            {
+                ID++;
+            }
+            return ID;
+        }
+
+        public void Add(decimal cash)
+        {
+            Balance+=cash;
+            Console.WriteLine($"Done! Balance: {Balance}");
+        }
+        public void Lower(decimal cash)
+        {
+            if(Balance > 0)
+            {
+                if(Balance - cash > 0)
+                {
+                    Balance -= cash;
+                    Console.WriteLine($"Done! Balance: {Balance}");
+                }
+                else
+                {
+                    Console.WriteLine($"Not enougth money! Balance: {Balance}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Something is wrong!");
+            }
+        }
+
+    }
+    public class Buiding
+    {
+        public Buiding() { }
+        public uint id, height, floors, flat_count, entrance_count;
+        private static HashSet<uint> Last_Random_ID = new HashSet<uint>( 0 ); 
+        public Buiding(uint id, uint height, uint floors, uint flat_count, uint entrance_count)
+        {
+            this.id = id;
+            this.height = height;
+            this.floors = floors;
+            this.flat_count = flat_count;
+            this.entrance_count = entrance_count;
+        }
+        public uint new_id()
+        {
+            Random r = new Random();
+            id = (uint)r.Next(0, 255);
+            if (!Last_Random_ID.Contains(id))
+            {
+                id++;
+            }
+            return id;
+        }
+       
+        public void HeightOfFloor()
+        {
+            Console.WriteLine($"Height of floor is: {(double)height/(double)floors}");
+        }
+        public void FlatCountPerEntrance()
+        {
+            Console.WriteLine($"Flats per entrance: {flat_count/entrance_count}");
+        }
+        public void FlatCountPerFloor()
+        {
+            Console.WriteLine($"Flats per floor : {flat_count / entrance_count/floors}");
+        }
+        public void Print() => Console.WriteLine($"ID: {id}\nHeight: {height}\nFloors: {floors}\nAmount of flats: {flat_count}\nAmount of entrances: {entrance_count}");
+        //public void InputB(out uint Id, out uint Height, out uint Floors, out uint Flat_Count, out uint Entrance_Count)
+        //{
+        //    Console.WriteLine("Enter ID");
+        //    Id = Convert.ToUInt32(Console.ReadLine());
+        //    Console.WriteLine("Enter Height");
+        //    Height = Convert.ToUInt32(Console.ReadLine());
+        //    Console.WriteLine("Enter Floors");
+        //    Floors = Convert.ToUInt32(Console.ReadLine());
+        //    Console.WriteLine("Enter Amount of flats");
+        //    Flat_Count = Convert.ToUInt32(Console.ReadLine());
+        //    Console.WriteLine("Enter Amount of entrances");
+        //    Entrance_Count = Convert.ToUInt32(Console.ReadLine());
+
+        //}
+
+    }
+    internal class Program
+    {
+        static void new_task()
+        {
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void Main(string[] args)
+        {
+
+            // Task7.1 CW
+            Bank_Account account = new Bank_Account(123,22800,Bank_Account.Acc_Type.Сберегательный);
+            account.Print();
+            new_task();
+
+            //Task7.2 CW
+            byte new_ID = account.new_ID();
+            Bank_Account account1 = new Bank_Account(new_ID,228000,Bank_Account.Acc_Type.Накопительный);
+            account1.Print();
+            new_task();
+
+            //Task7.3 CW
+            new_ID = account1.new_ID();
+            Bank_Account account2 = new Bank_Account(new_ID, 0, Bank_Account.Acc_Type.Сберегательный);
+            account2.Print();
+            account2.Add(1000);
+            Console.WriteLine();
+            account2.Lower(2000);
+            Console.WriteLine();
+            account2.Lower(500);
+            Console.WriteLine();
+            new_task();
+
+            //Task7.1 HW
+
+            Buiding building1 = new Buiding(100,100,20,400,5);
+            building1.Print();
+            building1.HeightOfFloor();
+            building1.FlatCountPerEntrance();
+            building1.FlatCountPerFloor();
+            new_task();
+
+            uint new_id = building1.new_id();
+            Buiding building2 = new Buiding(new_id, 200, 50,1000, 5);
+            building2.Print();
+            building2.HeightOfFloor();
+            building2.FlatCountPerEntrance();
+            building2.FlatCountPerFloor();
+            new_task();
+        }
+    }
+}
